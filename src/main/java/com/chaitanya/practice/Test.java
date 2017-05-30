8e05bcc33ec34ccc199379c4ca3681c65c8e9ae9
Chaitanya <kulk.cd@gmail.com>
2017-05-30 22:32:57 +0530

    intial commit

---
+++ src/main/java/com/chaitanya/practice/Test.java
@@ -0,0 +1,11 @@
+package com.chaitanya.practice;
+
+/**
+ * Created by Chaitanya on 30-05-2017.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+}
